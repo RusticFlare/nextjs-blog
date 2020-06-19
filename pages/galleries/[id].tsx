31db43be94f7c3566458ad33c02bf9e1bf7d6393
James Baker <j_d_baker@hotmail.com>
2020-06-19 18:42:58 +0100

    Start proper gallery pages

---
+++ pages/galleries/[id].tsx
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from 'components/layout'
+import ImageGallery from "components/image-gallery"
+import { getAllPhotos, getAllGalleryIds } from 'lib/graph-cms'
+import { GetStaticProps, GetStaticPaths } from 'next'
+
+export default function Gallery({ photos }) {
+  return (
+    <Layout>
+      <Head>
+        <title>A Gallery</title>
+      </Head>
+      <ImageGallery photos={photos} />
+    </Layout>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = await getAllGalleryIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const photos = await getAllPhotos()
+  return {
+    props: {
+      photos
+    }
+  }
+}
