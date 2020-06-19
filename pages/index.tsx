d8de3e1e988c9e77071addb5432c02cfae8f37a7
James Baker <j_d_baker@hotmail.com>
2020-06-19 19:53:24 +0100

    New homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,49 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from 'components/layout'
-import ImageGallery from "components/image-gallery"
-import { getAllPhotos } from 'lib/graph-cms'
+import { getAllGalleries } from 'lib/graph-cms'
+import GraphImage from 'graphcms-image'
+import Link from 'next/link'
 
-export default function Home({ photos }) {
+export default function Home({ galleries }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <ImageGallery photos={photos} />
+      <div
+        style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          margin: '0 auto',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {galleries.map(gallery => (
+          <Link href="/galleries/[id]" as={`/galleries/${gallery.id}`} >
+            <a>
+              <GraphImage
+                image={gallery.image}
+                maxWidth={500}
+                style={{
+                  width: '18rem',
+                  height: '18rem',
+                  margin: '32px 16px',
+                }}
+              />
+            </a>
+          </Link>
+        ))}
+      </div>
     </Layout>
   )
 }
 
 export async function getStaticProps() {
-  const photos = await getAllPhotos()
+  const galleries = await getAllGalleries()
   return {
     props: {
-      photos: photos
+      galleries: galleries
     }
   }
 }
